08a6c5879126b501d2d98481c60b9d3d2d148847
Yoichi Fujimoto <wozozo@gmail.com>
2016-11-01 20:32:36 +0900

    0.0.6

--- lib/Payjp.php
+++ lib/Payjp.php
@@ -16,7 +16,7 @@ class Payjp
     // @var boolean Defaults to true.
     public static $verifySslCerts = true;
 
-    const VERSION = '0.0.2';
+    const VERSION = '0.0.6';
 
     /**
      * @return string The API key used for requests.
